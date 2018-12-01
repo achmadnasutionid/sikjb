70a13a41b52ec74657346953048b25d7f37eb52b
Achmad Nasution <achmadynasution@gmail.com>
2018-12-01 10:36:45 +0700

    starter

---
+++ aplikasi/sikjb/src/main/java/com/sikjb/SikjbApplication.java
@@ -0,0 +1,12 @@
+package com.sikjb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikjbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikjbApplication.class, args);
+	}
+}
